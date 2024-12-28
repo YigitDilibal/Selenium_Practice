e87cd865d30c2eb41a7473c99512b4d1c36031f2
YigitDilibal <archery10@outlook.com>
2024-12-28 17:24:11 +0200

    first commit

---
+++ src/test/java/day01/C01_firstClass.java
@@ -0,0 +1,18 @@
+package day01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_firstClass {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.wisequarter.com/");
+        Thread.sleep(5000); //bir sonraki işlemi yapmak için 2000 ms yani 2 sn bekler
+        driver.quit();
+    }
+
+
+    }
+
